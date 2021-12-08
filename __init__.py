8f3c03f0980f8af43d79c41ed9ef62159c1ec28b
Hai Vu <49068114+haidepzai@users.noreply.github.com>
2021-12-08 19:58:00 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,6 +4,19 @@ from mycroft import MycroftSkill, intent_file_handler
 class NextcloudCalender(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
+        
+        
+    def initialize(self):
+        self.log.info('Collecting login information')
+
+        self.user = self.settings.get('username')
+        if not self.user:
+            self.log.info('Failed to retrieve username')
+
+        self.password = self.settings.get('password')
+        if not self.password:
+            self.log.info('failed to retrieve password')
+            
 
     @intent_file_handler('calender.nextcloud.intent')
     def handle_calender_nextcloud(self, message):
